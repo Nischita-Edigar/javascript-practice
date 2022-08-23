7c788e5455c66035fe43a91c5771b1c02fc59c6b
Nischita-Edigar <nischitav702@gmail.com>
2022-08-23 15:50:59 +0530

    javascript

--- DOM&EVENTS/Form validation/form.js
+++ DOM&EVENTS/Form validation/form.js
@@ -1,22 +1,43 @@
 function validate() {
     let name=document.getElementById('ename').value
-    if (name == "" && name.length<=4) {
-        document.getElementById('ename_message').innerHTML="please Enter minimum 5 characters"     
-    
+    if (name == "" ) {
+        document.getElementById('ename_message').innerHTML=" **please Enter valid Name"  
+        return false   
+    }   
+    if (name.length <=4 || name.length>=15 ) {
+        document.getElementById('ename_message').innerHTML="**please Enter minimum 5 characters"  
+        return false   
     }   
     
 
     let mail=document.getElementById('eemail').value
     if (mail ==""  ) {
-        document.getElementById('eemail_message').innerHTML="please Enter valid Email id"
-        
+        document.getElementById('eemail_message').innerHTML="**please Enter valid Email id"    
+        return false
     }
+    /*if (mail.indexOf('@')<=0 ) {
+        document.getElementById('eemail_message').innerHTML="**Invalid Email id"    
+        return false
+    }
+    if ((mail.charAt(mail.length-4)!='.'  ) &&  (mail.charAt(mail.length-3)!='.')) {
+        document.getElementById('eemail_message').innerHTML="**Invalid Email id"   
+        return false 
+    }*/
+
     let num=document.getElementById('ephone').value
-    if(num=="" && num.length<10) {
-        document.getElementById('enum_message').innerHTML="please Enter Valid 10-digit Mobile Number"
-        
+    if(num=="" ) {
+        document.getElementById('enum_message').innerHTML="**please Enter Valid 10-digit Mobile Number"
+        return false    
     }
-    return false
+    if(isNaN(num)) {
+        document.getElementById('enum_message').innerHTML="**please Enter digits only"
+        return false    
+    }
+    if(num.length!=10) {
+        document.getElementById('enum_message').innerHTML="**please Enter 10-digits only"
+        return false    
+    }
+    
    
      
 }
