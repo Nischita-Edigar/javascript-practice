1d7755567bac062d16eca58eace38cd3a37cd73d
Nischita-Edigar <nischitav702@gmail.com>
2022-09-12 10:26:21 +0530

    javascript-ES6

---
+++ ES6/spread/examples.js
@@ -0,0 +1,12 @@
+//Example 1
+let a = [1,2,3,4,5]
+let b= [...a,10,20,30]
+console.log(a);
+console.log(b);
+
+//Example 2
+let size1 =['S','M','L']
+let size2=['xl','xxl','xxxl']
+let size=[...size1,...size2]
+console.log(size);
+
